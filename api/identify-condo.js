c1786ee627bb1459c77ecfe99c0f1821071222a8
vsn-imoveis <vsnimoveis@gmail.com>
2026-09-18 16:30:52 -0300

    Allow browser test access to condo API

--- api/identify-condo.js
+++ api/identify-condo.js
@@ -1,4 +1,12 @@
 export default async function handler(req,res){
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers','Content-Type');
+
+  if(req.method==='OPTIONS'){
+    return res.status(204).end();
+  }
+
   try{
     if(req.method!=='POST') return res.status(405).json({error:'Método não permitido'});
 
